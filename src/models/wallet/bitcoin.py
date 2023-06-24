4c62a4e1b37096d504356cc39d6d5f98361f932a
arnosk <arnokemner@gmail.com>
2023-06-24 20:59:25 +0200

    Get all txs, with multiple url requests
    increase the offset, when there are more txs than the limit

--- src/models/wallet/bitcoin.py
+++ src/models/wallet/bitcoin.py
@@ -10,6 +10,7 @@ import logging
 
 from src.data.dbschemadata import Site, Transaction
 from src.data.dbschematypes import SiteType
+from src.data.types import Timestamp
 from src.db.db import Db
 from src.func.helperfunc import convert_timestamp
 from src.models.sitemodel import SiteModel
@@ -39,54 +40,65 @@ class Bitcoin(SiteModel):
 
 
 def _get_transactins_blockchaininfo(
-    accounts: list[str],
+    accounts: list[str], last_time: Timestamp = Timestamp(0)
 ) -> dict[str, tuple[bool, float]]:
-    """May raise RemoteError or KeyError"""
+    """May raise RemoteError or KeyError
+    First tx from blockchain.info is newest
+    """
     transactions = {}
-    # params = "|".join(accounts)
     for acc in accounts:
-        resp = request_get_dict(
-            url=f"https://blockchain.info/rawaddr/{acc}",
-            handle_429=True,
-            # If we get a 429 then their docs suggest 10 seconds
-            # https://blockchain.info/
-            backoff_in_seconds=15,
-        )
+        finished = False
+        limit = 10
+        offset = 0
+        tx_i = 0
+        tx_time = 1
+        while not finished:  # tx_time > int(last_time):
+            offset = tx_i
+            params = f"limit={limit}&offset={offset}"
 
-        n_tx = resp["n_tx"]
-        balance = resp["final_balance"]
-        print(f"Address {acc} has {n_tx} txs, balance = {balance}")
-        i = 0
-        for tx in resp["txs"]:
-            i = i + 1
-            # print(f"TX {i}: {tx}")
-            tx_in = False
-            tx_out = False
-            tx_fee = 0
-            tx_value = 0
-            address_in = ""
-            address_out = ""
-            tx_time = tx["time"]
-            for input in tx["inputs"]:
-                # print(f"input: {input}")
-                address_in = input["prev_out"]["addr"]
-                if address_in == acc:
-                    tx_in = True
-                    tx_fee = tx["fee"]
-                    tx_value = input["value"]
-
-            for output in tx["out"]:
-                # print(f"output {output}")
-                addr = output.get("addr", "unknown")
-                if addr == acc:
-                    tx_out = True
-                    address_out = addr
-                    tx_value = output["value"]
-
-            timestr = convert_timestamp(tx_time)
-            print(
-                f"tx {i}: {timestr} in {tx_in}, out {tx_out}, fee {tx_fee}, value {tx_value}, "
-                f"From {tx_in} {address_in} to {tx_out} {address_out} "
+            resp = request_get_dict(
+                url=f"https://blockchain.info/rawaddr/{acc}?{params}",
+                handle_429=True,
+                # If we get a 429 then their docs suggest 10 seconds
+                # https://blockchain.info/
+                backoff_in_seconds=60,
             )
+
+            n_tx = resp["n_tx"]
+            balance = resp["final_balance"]
+            print(f"Address {acc} has {n_tx} txs, balance = {balance}")
+            for tx in resp["txs"]:
+                tx_i = tx_i + 1
+                # print(f"TX {tx_i}: {tx}")
+                tx_in = False
+                tx_out = False
+                tx_fee = 0
+                tx_value = 0
+                address_in = ""
+                address_out = ""
+                tx_time = tx["time"]
+                for input in tx["inputs"]:
+                    # print(f"input: {input}")
+                    address_in = input["prev_out"]["addr"]
+                    if address_in == acc:
+                        tx_in = True
+                        tx_fee = tx["fee"]
+                        tx_value = input["value"]
+
+                for output in tx["out"]:
+                    # print(f"output {output}")
+                    addr = output.get("addr", "unknown")
+                    if addr == acc:
+                        tx_out = True
+                        address_out = addr
+                        tx_value = output["value"]
+
+                timestr = convert_timestamp(tx_time)
+                print(
+                    f"tx {tx_i}: {timestr} in {tx_in}, out {tx_out}, fee {tx_fee}, value {tx_value}, "
+                    f"From {tx_in} {address_in} to {tx_out} {address_out} "
+                )
+
+            finished = tx_i > n_tx or tx_time < int(last_time)
 
     return transactions
